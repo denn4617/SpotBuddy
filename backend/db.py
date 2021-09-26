6e1e6efdddcba2906414baca43d85bbe02897d18
Norspang <cnorsp19@student.aau.dk>
2021-09-26 02:49:42 +0200

    Working on Database setup, starting a bit on it

--- backend/db.py
+++ backend/db.py
@@ -13,7 +13,8 @@ Needs to be added:
  - all the features
 
 """
-
+import psycopg2
+from config import config
 
 class NotFoundError(Exception):
     pass
@@ -26,6 +27,42 @@ class NotAuthorizedError(Exception):
 class InternalServerError(Exception):
     pass
 
+class DBHandler():
+    connection = None
+    def connect(self):
+        try:
+            params = config()
+            self.connection = psycopg2.connect(**params)
+            self.cursor = self.connection.cursor()
+        
+        except (Exception) as error:
+            print(error)
+            self.disconnect()
+
+#         finally:
+#             if (self.connection):
+#                 self.cursor.close()
+#                 self.connection.close()
+#                 print("PostgreConnection Closed")
+# 
+    def disconnect(self):
+        if(self.connection):
+            self.cursor.close()
+            self.connection.close()
+
+    def test_connect(self):
+        if (self.connection):
+            print("Connection Works")
+        else:
+            print("Connection Not open, ya idiot")
+
+    def get_all_users(self):
+        self.cursor.execute("SELECT * FROM user_table")
+        return self.cursor.fetchall()
+    
+    def get_user_by_id(self,id):
+        self.cursor.execute("SELECT * FROM user_table WHERE user_id = %s", (id,))
+        return self.cursor.fetchone()
 
 def some_function(id: int=666):
     return {"Message": f"This is a test/placeholder function id: {id}"}
@@ -37,3 +74,9 @@ def login():
 
 def register():
     pass
+
+
+hander = DBHandler()
+hander.connect()
+print(hander.get_user_by_id(1))
+hander.disconnect()
