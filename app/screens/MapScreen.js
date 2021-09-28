1b220ac4c47cb8e35dbc421fcc53ef831be6e875
Dennis Kilic <dkilia19@student.aau.dk>
2021-09-29 00:17:30 +0200

    FUCK THIS.STATE HDSHFKJDSFIOALGKBRAEUIPFLKGBAGPORGBPAHIORGAINEOFPAGJEN9potiytr9w8tioqhe9apgoap

--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -6,15 +6,39 @@ import { Component } from "react";
 
 
 const MapScreen = () => {
-  let dataSpots;
-  fetch('http://localhost/api/spots')
-    .then(response => response.json())
-    .then(data => dataSpots = data);
+
+  getSpots = () => {
+    try {
+      axios.get("localhost:5000/api/spots")
+        .then(response => {
+          this.setState({ markers: response });
+          console.log(this.state.markers);
+        })
+        .catch(error => {
+          console.log('Error fetching and parsing data', error);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <MapView style={styles.map}>
+      <MapView
+        style={styles.map}
+        showsUserLocation={true}
+        /*initialRegion={{
+          latitude: this.state.latitude,
+          longitude: this.state.longitude,
+          latitudeDelta: 0.0462,
+          longitudeDelta: 0.0261,
+        }}*/>
 
+        {this.state.markers.map(marker => (
+          <Marker
+            coordinate={{ latitude: marker.member.latitude, longitude: marker.member.longitude }}
+          />
+        ))}
 
         <MapView.Marker
           key={2}
@@ -23,14 +47,7 @@ const MapScreen = () => {
           description={"test descrip"}
         />
 
-        {dataSpots => (
-          <Marker
-            key={index}
-            coordinate={marker.latlng}
-            title={marker.title}
-            description={marker.description}
-          />
-        )}
+
 
       </MapView>
 
